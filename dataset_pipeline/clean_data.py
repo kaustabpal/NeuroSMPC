8900ba8e232ecd1f9a91f860625106155418aa53
Kaustab Pal <kaustab21@gmail.com>
2023-02-15 15:15:39 +0530

    added auto renaming to clean_data.py

--- dataset_pipeline/clean_data.py
+++ dataset_pipeline/clean_data.py
@@ -5,12 +5,13 @@ import time
 import matplotlib.pyplot as plt
 import os
 import shutil
+from natsort import natsorted, ns
 
-dataset_dir = "occ_map/"
-plot_im_dir = "plot_im/"
-mean_dir = "mean_controls/"
-train_dir = "train/"
-test_dir = "test/"
+dataset_dir = "/Users/kaustabpal/work/carla_latest/occ_map/"
+plot_im_dir = "/Users/kaustabpal/work/carla_latest/plot_im/"
+mean_dir = "/Users/kaustabpal/work/carla_latest/mean_controls/"
+
+
 
 files = os.listdir(plot_im_dir)
 # initialising pygame
@@ -18,12 +19,12 @@ pygame.init()
 # creating display
 display = pygame.display.set_mode((640, 480))
 
-for i in range(2,len(files)-1):
+for i in range(len(files)-1):
     
     obs_pos = []
-    bev_file_name = dataset_dir + "data_" + str(i).zfill(2) + ".pkl"
-    im_file_name = plot_im_dir + "data_" + str(i) + ".png"
-    mean_file_name = mean_dir + "data_" + str(i) + ".npy"
+    bev_file_name = dataset_dir + "data_" + str(i).zfill(5) + ".pkl"
+    im_file_name = plot_im_dir + "data_" + str(i).zfill(5) + ".png"
+    mean_file_name = mean_dir + "data_" + str(i).zfill(5) + ".npy"
 
     img =  pygame.image.load(im_file_name).convert() #cv2.imread(file_name) #print(f[:3])
 
@@ -60,15 +61,18 @@ for i in range(2,len(files)-1):
                     flag = False
                     quit()
 
-    #         # checking if key "J" was pressed
-    #         if event.key == pygame.K_j:
-    #             print("Key J has been pressed")
+plt_files = os.listdir(plot_im_dir)
+mean_files = os.listdir(mean_dir)
+dataset_files = os.listdir(dataset_dir)
 
-    #         # checking if key "P" was pressed
-    #         if event.key == pygame.K_p:
-    #             print("Key P has been pressed")
+plt_files_sorted = natsorted(plt_files, key=lambda y: y.lower())
+mean_files_sorted = natsorted(mean_files, key=lambda y: y.lower())
+dataset_files_sorted = natsorted(dataset_files, key=lambda y: y.lower())
 
-    #         # checking if key "M" was pressed
-    #         if event.key == pygame.K_m:
-    #             print("Key M has been pressed")
-    # cv2.destroyAllWindows() # destroys the window showing image
+for i in range(len(plt_files_sorted)):
+    os.replace(plot_im_dir+plt_files_sorted[i], plot_im_dir+"data_"+str(i).zfill(5) + ".png")
+    os.replace(mean_dir+mean_files_sorted[i], mean_dir+"data_"+str(i).zfill(5) + ".npy")
+    os.replace(dataset_dir+dataset_files_sorted[i], dataset_dir+"data_"+str(i).zfill(5) + ".pkl")
+    # print("######################################################################################")
+
+# quit()
