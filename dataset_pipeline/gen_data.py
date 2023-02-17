010a9b07662d2ea6146615dee2055d0e8b4a0324
Parth Shah <parths.2410@gmail.com>
2023-02-17 21:18:53 +0530

    gen_data file updated

--- dataset_pipeline/gen_data.py
+++ dataset_pipeline/gen_data.py
@@ -1,5 +1,5 @@
 import torch
-from goal_sampler_static_obs import Goal_Sampler
+from dataset_pipeline.goal_sampler_static_obs import Goal_Sampler
 from matplotlib import pyplot as plt
 from matplotlib import pyplot as plt
 import numpy as np
@@ -8,9 +8,9 @@ import time
 import matplotlib.pyplot as plt
 import pickle
 import os
-from utils import rotate
-from utils import draw_circle
-from frenet_transformations import global_traj, global_to_frenet, frenet_to_global
+from dataset_pipeline.utils import rotate
+from dataset_pipeline.utils import draw_circle
+from dataset_pipeline.frenet_transformations import global_traj, global_to_frenet, frenet_to_global
 import argparse
 torch.manual_seed(42)
 
@@ -48,70 +48,74 @@ def run():
     files = os.listdir(dataset_dir)
     print(len(files)-1)
     for i in range(0,len(files)):
-        t_1 = time.time()
-        print(i)
-        obs_pos = []
-        file_name = dataset_dir + "data_" + str(i).zfill(2) + ".pkl"
-        plt_save_file_name = plot_im_dir + "data_" + str(i)
-        mean_save_filename = mean_dir + "data_" + str(i)
-        with open(file_name, "rb") as f:
-            data = pickle.load(f)
-        obs = data['obstable_array'] # obstacle pos in euclidean space
-        
-        g_path = data['g_path'] -[256/2,256/2] # global path points
-        g_path = g_path[:, [1,0]]*30/256
-        
-        obs_pos = np.array(to_continuous(obs))
+        for velocity in range(1, 6):
+            t_1 = time.time()
+            print(i)
 
-        new_g_path, interpolated_g_path, theta = global_traj(g_path, 0.1)
+            data_instance =  str(i).zfill(2) + "_" + str(int(velocity))
 
-        ego_theta = np.rad2deg(np.pi/2 + (np.pi/2 - theta[0]))
-        # print(ego_theta)
-        
-        obs_pos_frenet = global_to_frenet(obs_pos, new_g_path, interpolated_g_path)
-
-        sampler = Goal_Sampler(torch.tensor([0,0,np.deg2rad(ego_theta)]), 4.13, 0, obstacles=obs_pos_frenet)
-        t1 = time.time()
-        sampler.plan_traj()
-        # print("Planning time: ", time.time()-t1)
-
-        mean_controls = sampler.mean_action
-        mean_traj = sampler.traj_N[-2,:,:]
-        cov_controls = sampler.scale_tril
-        # print(mean_controls)
-        
-        mean_controls[:,1] = frenet_to_global(mean_traj, new_g_path, interpolated_g_path, 0.1)
-        # print(mean_controls)
-        # quit()
-        sampler.obstacles = obs_pos
-        sampler.mean_action = torch.as_tensor(mean_controls)
-        sampler.c_state = torch.tensor([0,0,np.deg2rad(90)])
-        sampler.infer_traj()
-        np.save(mean_save_filename,sampler.mean_action)
-        
-        ## plot
-        # for k in range(g_path.shape[0]):
-        plt.scatter(g_path[:,0],g_path[:,1],color='blue', alpha=0.1)
-
-        x_car, y_car = draw_circle(0, 0, 1.80)
-        plt.plot(x_car,y_car,'g')
-        
-        # for j in range(obs_pos.shape[0]):
-        # print(obs_pos[:][:,0])
-        
-        plt.plot(obs_pos[:,0], obs_pos[:,1], 'k.')
-        plt.scatter(obs_pos_frenet[:,0], obs_pos_frenet[:,1], color='orange')
+            obs_pos = []
+            file_name = dataset_dir + "data_" + str(i).zfill(2) + ".pkl"
+            plt_save_file_name = plot_im_dir + "data_" + data_instance
+            mean_save_filename = mean_dir + "data_" + data_instance
+            with open(file_name, "rb") as f:
+                data = pickle.load(f)
+            obs = data['obstable_array'] # obstacle pos in euclidean space
             
-        # for j in range(sampler.traj_N.shape[0]):
-        plt.plot(sampler.traj_N[:,:,0], sampler.traj_N[:,:,1], 'r', alpha=0.1)
-        plt.plot(sampler.traj_N[-2,:,0], sampler.traj_N[-2,:,1], 'g')
-        plt.plot(sampler.top_trajs[0,:,0], sampler.top_trajs[0,:,1], 'blue')
-        print("Total time: ", time.time()-t_1)
-        # print(sampler.top_trajs[0,:,:2])
-        plt.savefig(plt_save_file_name)
-        # plt.show()
-        plt.clf()
-        # quit()
+            g_path = data['g_path'] -[256/2,256/2] # global path points
+            g_path = g_path[:, [1,0]]*30/256
+            
+            obs_pos = np.array(to_continuous(obs))
+
+            new_g_path, interpolated_g_path, theta = global_traj(g_path, 0.1)
+
+            ego_theta = np.rad2deg(np.pi/2 + (np.pi/2 - theta[0]))
+            # print(ego_theta)
+            
+            obs_pos_frenet = global_to_frenet(obs_pos, new_g_path, interpolated_g_path)
+
+            sampler = Goal_Sampler(torch.tensor([0,0,np.deg2rad(ego_theta)]), 4.13, 0, obstacles=obs_pos_frenet)
+            t1 = time.time()
+            sampler.plan_traj()
+            # print("Planning time: ", time.time()-t1)
+
+            mean_controls = sampler.mean_action
+            mean_traj = sampler.traj_N[-2,:,:]
+            cov_controls = sampler.scale_tril
+            # print(mean_controls)
+            
+            mean_controls[:,1] = frenet_to_global(mean_traj, new_g_path, interpolated_g_path, 0.1)
+            # print(mean_controls)
+            # quit()
+            sampler.obstacles = obs_pos
+            sampler.mean_action = torch.as_tensor(mean_controls)
+            sampler.c_state = torch.tensor([0,0,np.deg2rad(90)])
+            sampler.infer_traj()
+            np.save(mean_save_filename,sampler.mean_action)
+            
+            ## plot
+            # for k in range(g_path.shape[0]):
+            plt.scatter(g_path[:,0],g_path[:,1],color='blue', alpha=0.1)
+
+            x_car, y_car = draw_circle(0, 0, 1.80)
+            plt.plot(x_car,y_car,'g')
+            
+            # for j in range(obs_pos.shape[0]):
+            # print(obs_pos[:][:,0])
+            
+            plt.plot(obs_pos[:,0], obs_pos[:,1], 'k.')
+            plt.scatter(obs_pos_frenet[:,0], obs_pos_frenet[:,1], color='orange')
+                
+            # for j in range(sampler.traj_N.shape[0]):
+            plt.plot(sampler.traj_N[:,:,0], sampler.traj_N[:,:,1], 'r', alpha=0.1)
+            plt.plot(sampler.traj_N[-2,:,0], sampler.traj_N[-2,:,1], 'g')
+            plt.plot(sampler.top_trajs[0,:,0], sampler.top_trajs[0,:,1], 'blue')
+            print("Total time: ", time.time()-t_1)
+            # print(sampler.top_trajs[0,:,:2])
+            plt.savefig(plt_save_file_name)
+            # plt.show()
+            plt.clf()
+            # quit()
 
 
 
