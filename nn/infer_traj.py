5b3e362867a4179afe77af6c3a7a0743a5a944c2
Kaustab Pal <kaustab21@gmail.com>
2023-03-18 21:50:27 +0530

    minor changes

--- nn/infer_traj.py
+++ nn/infer_traj.py
@@ -3,6 +3,7 @@ from dataset_pipeline.goal_sampler_static_obs import Goal_Sampler
 from dataset_pipeline.grad_cem import GradCEM
 from dataset_pipeline.frenet_transformations import global_traj, global_to_frenet, frenet_to_global
 from matplotlib import pyplot as plt
+import matplotlib.patches as patches
 from matplotlib import pyplot as plt
 import numpy as np
 np.set_printoptions(suppress=True)
@@ -21,16 +22,15 @@ import copy
 
 @dataclass
 class Args:
-    dataset_dir: str = '/Users/kaustabpal/work/iros_23/5k_data/test/' # occ_map/' #'../carla_latest/' # 'data/dataset_beta/'
-    # dataset_dir: str = '/Users/kaustabpal/Downloads/experiments_22-02-2023/NuroMPPI_2-1/' # occ_map/' #'../carla_latest/' # 'data/dataset_beta/'
-    weights_dir: str = '../iros_23/weights/' 
-    loss_dir: str = '../iros_23/loss/' 
-    infer_dir: str = "/Users/kaustabpal/work/iros_23/5k_data/test/infer_dir/"
+    dataset_dir: str = '/Users/kaustabpal/work/iros_23/5k_data/train/'
+    weights_dir: str = '../iros_23/weights/'
+    loss_dir: str = '../iros_23/loss/'
+    infer_dir: str = "/Users/kaustabpal/work/iros_23/5k_data/train/infer_dir/"
     time_dir: str = "../iros_23/time_dir/"
     val_split: float = 0.3
     num_epochs: int = 1000
     seed: int = 12321
-    exp_id: str = 'exp10'
+    exp_id: str = 'exp19'
 args = tyro.cli(Args)
 
 def to_continuous(obs):
@@ -65,6 +65,7 @@ def run():
     gradcem_time = []
     
     for i in range(0,len(occ_map_files)):
+        ax = plt.gca()
         t_1 = time.time()
         print(i)
         obs_pos = []
@@ -105,7 +106,7 @@ def run():
             tic = time.time()
             nmppi.mean_action = model(occ_map.unsqueeze(0)).reshape(30,2) # NN output reshaped
             toc = time.time()
-            print(toc-tic)
+            # print(toc-tic)
             # quit()
             nmppi.mean_action[:,0] = nmppi.mean_action[:,0]*torch.tensor([4.13], device=device)
         nmppi.infer_traj()
@@ -113,18 +114,18 @@ def run():
         nn_time.append(t2-t1)
         print("Neuro MPPI Inference time: ", t2-t1)
 
-        t1 = time.time()
-        tic = time.time()
-        new_g_path, interpolated_g_path, theta = global_traj(g_path, 0.1)
-        # print("global_traj: ", time.time() - tic)
-        ego_theta = np.rad2deg(np.pi/2 + (np.pi/2 - theta[0]))
-        tic = time.time()
-        obs_pos_frenet = global_to_frenet(obs_pos, new_g_path, interpolated_g_path)
-        print("global_to_frenet: ", time.time() - tic)
-        tic = time.time()
-        mppi = Goal_Sampler(torch.tensor([0,0,np.deg2rad(90)]), 4.13, 0, obstacles=obs_pos, num_particles = 100)
-        print("Goal_Sampler: ", time.time() - tic)
-        tic = time.time()
+        # t1 = time.time()
+        # tic = time.time()
+        # new_g_path, interpolated_g_path, theta = global_traj(g_path, 0.1)
+        # # print("global_traj: ", time.time() - tic)
+        # ego_theta = np.rad2deg(np.pi/2 + (np.pi/2 - theta[0]))
+        # tic = time.time()
+        # obs_pos_frenet = global_to_frenet(obs_pos, new_g_path, interpolated_g_path)
+        # print("global_to_frenet: ", time.time() - tic)
+        # tic = time.time()
+        # mppi = Goal_Sampler(torch.tensor([0,0,np.deg2rad(90)]), 4.13, 0, obstacles=obs_pos, num_particles = 100)
+        # print("Goal_Sampler: ", time.time() - tic)
+        # tic = time.time()
         # mppi.plan_traj()
         # print("plan_traj: ", time.time() - tic)
         # mean_controls1 = mppi.mean_action
@@ -133,16 +134,16 @@ def run():
         # mean_controls1[:,1] = frenet_to_global(mean_traj1, new_g_path, interpolated_g_path, 0.1)
         # mppi.obstacles = obs_pos
         # mppi.mean_action = torch.as_tensor(mean_controls1)
-        mppi.mean_action = mean_controls_gt
+        # mppi.mean_action = mean_controls_gt
         # np.save(mean_save_filename,mppi.mean_action)
         # mppi.c_state = torch.tensor([0,0,np.deg2rad(90)])
-        tic = time.time()
-        mppi.infer_traj()
+        # tic = time.time()
+        # mppi.infer_traj()
         # print("infer_traj: ", time.time() - tic)
         # quit()
-        t2 = time.time()
-        mppi_time.append(t2-t1)
-        print("MPPI Inference time: ", t2-t1)
+        # t2 = time.time()
+        # mppi_time.append(t2-t1)
+        # print("MPPI Inference time: ", t2-t1)
 
         # t1 = time.time()
         # new_g_path, interpolated_g_path, theta = global_traj(g_path, 0.1)
@@ -164,32 +165,37 @@ def run():
 
         plt.scatter(g_path[:,0],g_path[:,1],color='blue', alpha=0.1, label = "Global path")
 
-        x_car, y_car = draw_circle(0, 0, 1.80)
-        plt.plot(x_car,y_car,'g')
+        # x_car, y_car = draw_circle(0, 0, 1.80)
+        # plt.plot(x_car,y_car,'g')
+        rect = patches.Rectangle((-0.965, -2.345),
+                                 1.93, 4.69, linewidth=1,
+                                 edgecolor='r', facecolor='none')
+        # ax.add_patch(rect)
         
-        plt.plot(obs_pos[:,0], obs_pos[:,1], 'k.')
+        plt.plot(obs_pos[:, 0], obs_pos[:, 1], 'k.')
         
         # plt.plot(nmppi.traj_N[:,:,0], nmppi.traj_N[:,:,1], '.b', markersize=1, alpha=0.1)
         plt.plot(nmppi.traj_N[-2,:,0], nmppi.traj_N[-2,:,1], 'red', markersize=3.5, label = "NMPPI")
-        # plt.plot(nmppi.top_trajs[0,:,0], nmppi.top_trajs[0,:,1], 'green', markersize=2, label = "NMPPI Best traj")
+        plt.plot(nmppi.top_trajs[0,:,0], nmppi.top_trajs[0,:,1], 'green', markersize=2, label = "NMPPI Best traj")
         
-        plt.plot(mppi.traj_N[-2,:,0], mppi.traj_N[-2,:,1], 'blue', markersize=3, label = "MPPI")
+        # plt.plot(mppi.traj_N[-2,:,0], mppi.traj_N[-2,:,1], 'blue', markersize=3, label = "MPPI")
         # plt.plot(mppi.top_trajs[0,:,0], mppi.top_trajs[0,:,1], 'green', markersize=2, label = "MPPI Best traj")
         
         # plt.plot(gradcem.traj_N[-2,:,0].detach(), gradcem.traj_N[-2,:,1].detach(), 'blue', markersize=3, label = "GradCEM")
         # plt.plot(gradcem.top_trajs[0,:,0].detach(), gradcem.top_trajs[0,:,1].detach(), 'green', markersize=2, label = "GradCEM Best traj")
-        
+        ax.add_patch(rect)
         plt.ylim(-15,15)
         plt.xlim(-15,15)
         plt.legend(loc="lower center")
         plt.xticks([])
         plt.yticks([])
-        plt.savefig(infer_file_name)
+        plt.savefig(infer_file_name,bbox_inches='tight')
+        # plt.imsave(infer_file_name, pad_inches=0)
         # plt.show()
         plt.clf()
         # quit()
     nn_time = np.array(nn_time)
-    print(np.mean(nn_time))
+    # print(np.mean(nn_time))
     # np.save(args.time_dir+'nn_time', nn_time)
 
 
